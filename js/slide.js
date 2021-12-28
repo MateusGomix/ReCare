bfddfd90c9df10bd33bdfcea5106c43628b49646
bea <bia.irineu14@gmail.com>
2021-12-28 13:32:42 -0300

    glide e icone

--- js/slide.js
+++ js/slide.js
@@ -1,1 +1,13 @@
-const slide = document.querySelector("js-slide");
+const $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+
+new Glider($simpleCarousel, {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".js-carousel--simple-dots",
+    arrows: {
+        prev: ".js-carousel--simple-prev",
+        next: ".js-carousel--simple-next",
+    },
+});
