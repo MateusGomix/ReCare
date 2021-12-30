47b9d2c9f757b7c0b7f2d8dc15b5ca8ff6e63d2a
Daniel Paiva <danielpaiva1964@gmail.com>
2021-12-30 01:12:10 -0300

    Atualizei as funções que usam mysql
    As funções mysql_ foram descontinuadas e agora usam o padrão mysqli_, então fiz as devidas atuaizações.
    AVISO: Precisa ver a linha 41 pra ver se o modo STORE RESULT tá certo mesmo e o que ele significa

--- Páginas/PaginaCadastro.php
+++ Páginas/PaginaCadastro.php
@@ -10,57 +10,48 @@ $email = $_POST['email'];
 
 $senha = MD5($_POST['senha']);
 
-$connect = mysql_connect('localhost','UsuarioReCare','Senharecare123@');
+$connect = mysqli_connect('localhost','root','Deusefiel29');
 
-$db = mysql_select_db('ReCare');
+$db = mysqli_select_db($connect,'recare');
 
 $query_select = "SELECT Email FROM Pessoa WHERE Email = '$email'";
 
-$select = mysql_query($query_select,$connect);
+$select = mysqli_query($connect, $query_select);
 
-$array = mysql_fetch_array($select);
+$array = mysqli_fetch_array($select);
 
-/*$logarray = $array['Email'];
+$logarray = $array['Email'];
 
-  if($email == "" || $email == null)
-  {
+  if($email == "" || $email == null){
 
     echo"<script language='javascript' type='text/javascript'>alert('O campo email deve ser preenchido');window.location.href='PaginaCadastro.html';</script>";
 
-  }
-  else
-  {
+    }else{
 
-    if($logarray == $email)
-    {
+      if($logarray == $email){
 
         echo"<script language='javascript' type='text/javascript'>alert('Esse email já existe');window.location.href='PaginaCadastro.html';</script>";
 
         die();
 
-    }
-    else
-    {*/
+      }else{
 
         $query = "INSERT INTO Pessoa (Nome, DataNascimento, Telefone, Email, Senha) VALUES ('$nome','$nascimento','$telefone','$email','$senha')";
 
-        $insert = mysql_query($query,$connect);
+        $insert = mysqli_query($connect, $query, $result_mode = MYSQLI_STORE_RESULT);
 
-        if($insert)
-        {
+        if($insert){
 
           echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='email.html'</script>";
 
-        }
-        else
-        {
+        }else{
 
           echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='PaginaCadastro.html'</script>";
 
         }
 
-    /*}
+      }
 
-  }*/
+    }
 
 ?>
