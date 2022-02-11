01064aae18de497330692b383bb17a30457ba6c2
MateusGomix <gomes.mateus@unifesp.br>
2022-02-10 23:02:27 -0300

    Agora não é mais possível tentar inserir valores inválidos

--- Indexes/adicionaMedico.php
+++ Indexes/adicionaMedico.php
@@ -3,13 +3,28 @@ $mysql = new mysqli('localhost', 'root', '', 'recare');
 
 $cpfMed = $_POST['cpf'];
 
-$addMed = $mysql->prepare('INSERT INTO AtendeEm
+$query = "SELECT * FROM Pessoa p, Medico m
+          WHERE m.ID_Medico = p.ID_Pessoa 
+          AND p.CPF = '$cpfMed'";
+$aux = mysqli_query($mysql, $query, $result_mode = MYSQLI_STORE_RESULT);
+
+$query = "SELECT * FROM Pessoa p, Medico m, AtendeEm ae
+          WHERE m.ID_Medico = p.ID_Pessoa 
+          AND p.CPF = '$cpfMed'
+          AND ae.ID_Medico = m.ID_Medico";
+$aux2 = mysqli_query($mysql, $query, $result_mode = MYSQLI_STORE_RESULT);
+
+if(mysqli_num_rows($aux) > 0 && mysqli_num_rows($aux2) == 0){
+
+    $addMed = $mysql->prepare('INSERT INTO AtendeEm
                            VALUES((SELECT ID_Medico FROM Medico, Pessoa 
                            WHERE Pessoa.ID_Pessoa = Medico.ID_Medico AND Pessoa.CPF = ?),
                            (SELECT DISTINCT ID_Hospital FROM Hospital, Admin 
                            WHERE Hospital.ID_Admin = ?))');
-$addMed->bind_param('ss', $cpfMed, $_GET['idAdmin']);
-$addMed->execute();
+    $addMed->bind_param('ss', $cpfMed, $_GET['idAdmin']);
+    $addMed->execute();
+
+}
 
 header("Location: indexAdmin.php"); 
 exit();
--- Indexes/mudaCuidador.php
+++ Indexes/mudaCuidador.php
@@ -3,11 +3,20 @@ $mysql = new mysqli('localhost', 'root', '', 'recare');
 
 $cpfMed = $_POST['cpf'];
 
-$addMed = $mysql->prepare('UPDATE Paciente SET ID_Cuidador = 
-                           (SELECT DISTINCT ID_Pessoa FROM Pessoa WHERE CPF = ?)
-                           WHERE ID_Paciente = ?;');
-$addMed->bind_param('ss', $cpfMed, $_GET['idPaciente']);
-$addMed->execute();
+$query = "SELECT * FROM Pessoa p, Medico m
+          WHERE p.CPF = '$cpfMed'";
+$aux = mysqli_query($mysql, $query, $result_mode = MYSQLI_STORE_RESULT);
+
+
+if(mysqli_num_rows($aux) > 0){
+
+    $addMed = $mysql->prepare('UPDATE Paciente SET ID_Cuidador = 
+                            (SELECT DISTINCT ID_Pessoa FROM Pessoa WHERE CPF = ?)
+                            WHERE ID_Paciente = ?;');
+    $addMed->bind_param('ss', $cpfMed, $_GET['idPaciente']);
+    $addMed->execute();
+
+}
 
 header("Location: indexPaciente.php"); 
 exit();
